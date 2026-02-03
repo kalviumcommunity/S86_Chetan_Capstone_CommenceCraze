078316ec691ad19c7ea678604b6e7c8f8c0a6f4e
Thirumalasetty Chetan Rayalu <chetanrayaluthirumalasetty@gmail.com>
2026-02-03 10:58:47 +0530

    Fix frontend URL in CORS middleware

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL ||'https://commencecraze.vercel.app/',
+    origin: process.env.FRONTEND_URL ||'https://commencecraze.vercel.app',
     credentials: true
 }));
 app.use(express.json());
